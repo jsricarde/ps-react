b68545b36ffe2fb006f84a9c0cdf9b98f8045aa4
ricarde <jricarde@velocitypartners.net>
2018-04-03 16:27:47 -0500

    Add the files for Document module
      * Add all the files for the module.
      * Modify some files for works the docs autogerated
        functionality.

---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import HelloWorld from 'ps-react/HelloWorld';
+
+export default function ExampleHelloWorld() {
+  return <HelloWorld message="Pluralsight viewers!!" />
+}
